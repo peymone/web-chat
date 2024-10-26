6ba3842104a69839b48c89020cf07d8439c9fe99
Vladimir Reymer <vladimireymer@gmail.com>
2024-10-26 18:38:51 +0700

    Connection rework completed

--- app/chat/events.py
+++ app/chat/events.py
@@ -6,27 +6,23 @@ from datetime import datetime
 
 # My modules
 from .. import socketio
-from ..database import add_message, get_chats, get_messages
+from ..database import add_message, get_messages
 
 
 # Configuration variables
 date_time_format = "%I:%M %p %B %d"
-chats_from_db = get_chats()
-rooms_with_sid = dict()
+room_users = dict()
 
-
-def get_users_amount_per_chat() -> dict:
-    """Get amount of users in each chat"""
-    
-    return {chat: len(sids) for chat, sids in rooms_with_sid.items()}
+def get_users_amount_by_chat():
+    return {room: len(sids) for room, sids in room_users.items()}
 
 @socketio.on('connect')
-def on_connect(auth):
-    """Create connect log and call join room event on client"""
-    
+def on_connect():
+    """Create connect log and call join-room event on client"""
+        
     # Log connect message
     sid = request.sid
-    user = auth.get('user')
+    user = session.get('name')
     print(f"{user} connected with sid: '{sid}'")
     
     # Call event on client for join room
@@ -43,14 +39,16 @@ def on_join(user_data: dict):
     dpeartment: str = user_data.get('department')
     dpeartment_role: str = user_data.get('department_role')
     
-    # Create room and add sid
-    rooms_with_sid[room] = list()
-    rooms_with_sid[room].append(request.sid)
-    
-    # Join the rooom by it's name
+    # Join the rooom by it's name and save log
     join_room(room)
     print(f"{user} joined room: '{room}'")
     
+    # Count users in room
+    if room not in room_users:
+        room_users[room] = list()
+        
+    room_users[room].append(request.sid)
+            
     # Send messages history to new user
     messages: tuple = get_messages(chat_name=room)
     
@@ -65,60 +63,74 @@ def on_join(user_data: dict):
                     'time': message[2].strftime(date_time_format).lstrip('0')
                 }
                 
-                emit('receive-history', history, to=request.sid)
+                socketio.emit('receive-history', history, to=request.sid)
     
-    # Send message to room
-    if request.sid in rooms_with_sid[room]:
-        message = user + " has entered the chat"    
-        socketio.send(message, room=room)
+    # Send system message to room
+    socketio.send(user + " has entered the chat", room=room)
 
-@socketio.on('server-message')
+@socketio.on('receive-message')
 def on_message(user_data_and_message):
     
     # Parse data from client
+    user_data_and_message = user_data_and_message.get('user_data')
     email: str = user_data_and_message.get('email')
     user: str = user_data_and_message.get('user')
     room: str = user_data_and_message.get('room')
     dpeartment: str = user_data_and_message.get('department')
     dpeartment_role: str = user_data_and_message.get('department_role')
     message: str = user_data_and_message.get('message')
-    
-    # Broadcast message to chat room
-    if request.sid in rooms_with_sid[room]:
-        # Build message
-        now: str = datetime.now().strftime(date_time_format).strip('0')
-        message_data = {'name': user, 'msg': message, 'time': now}
+                                            
+    # Build message
+    now: str = datetime.now().strftime(date_time_format).strip('0')
+    message_data = {'name': user, 'msg': message, 'time': now}
         
-        # Send message and save it to database
-        socketio.emit('client-message', message_data, to=room)
-        add_message(user, room, message)
+    # Send message to room and save it to database
+    socketio.emit('receive-message', message_data, to=room)
+    add_message(user, room, message)
     
 @socketio.on('leave-room')
 def on_leave(user_data: dict):
-    
+    """Event triggered from client on leave-button click"""
+        
     # Parse data from client
+    user_data = user_data.get('user_data')
     email: str = user_data.get('email')
     user: str = user_data.get('user')
     room: str = user_data.get('room')
     dpeartment: str = user_data.get('department')
     dpeartment_role: str = user_data.get('department_role')
     
-    # Delete user sid from rooms and leave room
-    if request.sid in rooms_with_sid[room]:
-        rooms_with_sid[room].remove(request.sid)
-        leave_room(room)
+    # Leave room
+    leave_room(room)
+    
+    # Remove sid from room
+    if room in room_users and request.sid in room_users[room]:
+        room_users[room].remove(request.sid)
+        if len(room_users[room]) == 0:
+            del room_users[room]
         
-        # Call event on client side to clear session and redirect
-        emit('leave-room', {'url': '/rooms'}, to=request.sid)
+    # Call leave-room event on client side - redirect to '/rooms'
+    socketio.emit('leave-room', {'url': '/rooms'}, to=request.sid)
+        
+    # Send system message to room and save log
+    socketio.send(user + " has leaved the chat", room=room)
+    print(f"{user} leaved room: '{room}'")
+        
+@socketio.on('disconnect')
+def handle_disconnect():
+    """Event triggered on reload, close page.
+    When page is reloaded or closed - 'leave' event isn't triggered.
+    """
     
-socketio.on('disconnect')
-def on_disconnect():
-    """I don't know when this invoke. Just remove sid from rooms"""
+    user = session.get('name')
+    room = session.get('room')
+
+    # Remove sid from room
+    if room in room_users and request.sid in room_users[room]:
+        room_users[room].remove(request.sid)
+        if len(room_users[room]) == 0:
+            del room_users[room]
     
-    for room, sids in rooms_with_sid.items():
-        if request.sid in sids:
-            sids.remove(request.sid)
-            leave_room(room)
-    
-    name = session.get('name')
-    print(name + " disconnected")
+    # Send system message and save log
+    socketio.send(f"{user} has leaved the chat", room=room)
+    print(user + " disconnected")
